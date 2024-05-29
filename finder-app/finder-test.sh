923644f5ebd8267a84de726c97666951fadfdfba
yallaSany <sunnyyalla224@gmail.com>
2024-05-29 13:57:22 +0530

    Update finder-test.sh
    Updated script

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,73 +1,26 @@
-#!/bin/sh
-# Tester script for assignment 1 and assignment 2
-# Author: Siddhant Jajoo
+#!/bin/bash
 
-set -e
-set -u
-
-NUMFILES=10
-WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
-
-if [ $# -lt 3 ]
-then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
-else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+# Check if both arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Error: Two arguments are required."
+    echo "Usage: $0 <filesdir> <searchstr>"
+    exit 1
 fi
 
-MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
+filesdir=$1
+searchstr=$2
 
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
-
-rm -rf "${WRITEDIR}"
-
-# create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+# Check if filesdir is a valid directory
+if [ ! -d "$filesdir" ]; then
+    echo "Error: $filesdir is not a valid directory."
+    exit 1
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
-for i in $( seq 1 $NUMFILES)
-do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
-done
+# Count the number of files
+num_files=$(find "$filesdir" -type f | wc -l)
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# Count the number of matching lines
+num_matching_lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
 
-# remove temporary directories
-rm -rf /tmp/aeld-data
-
-set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
-if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
-else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
-fi
+# Print the result
+echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
